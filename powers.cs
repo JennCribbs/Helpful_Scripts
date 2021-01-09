2f6be9b7411fc368a8a5d78fb4ad614e23b0d0f0
Jennifer Cribbs <cribbs.jennifer@gmail.com>
2021-01-09 12:25:49 -0600

    Calculating Powers in C#

---
+++ powers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Math : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Mathf.Pow(2, 24));
+    }
+}
